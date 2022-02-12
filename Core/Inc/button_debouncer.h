9a8b32c3d3b2f74a3e2ea128adbb3b206b668d0d
Marian Szabo <mszabo@coralwave.com>
2022-02-12 08:59:59 -0500

    debounced keypad scaned by timer interrupt generates key pressed/released events

---
+++ Core/Inc/button_debouncer.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#define DebounceQLen  16
+
+enum ButtonEvent
+{
+	BUTTON_NO_CHANGE = -1,
+	BUTTON_PRESSED,
+	BUTTON_RELEASED,
+};
+
+
+class ButtonDebouncer
+{
+	bool DebounceCircularQueue[DebounceQLen],
+		*p_debounce;
+
+	bool button_pressed;
+	int running_total;
+
+public:
+	ButtonDebouncer ();
+	ButtonEvent update_with_sampling(bool sample);
+};
+
+
+
+
--- Core/Src/stm32f4xx_it.c
+++ Core/Src/stm32f4xx_it.c
@@ -22,6 +22,7 @@
 #include "stm32f4xx_it.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include "appmain.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -55,7 +56,7 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
-
+extern TIM_HandleTypeDef htim3;
 /* USER CODE BEGIN EV */
 
 /* USER CODE END EV */
@@ -197,6 +198,20 @@ void SysTick_Handler(void)
 /* For the available peripheral interrupt handler names,                      */
 /* please refer to the startup file (startup_stm32f4xx.s).                    */
 /******************************************************************************/
+
+/**
+  * @brief This function handles TIM3 global interrupt.
+  */
+void TIM3_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM3_IRQn 0 */
+
+  /* USER CODE END TIM3_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim3);
+  /* USER CODE BEGIN TIM3_IRQn 1 */
+	on_periodic_timer();
+  /* USER CODE END TIM3_IRQn 1 */
+}
 
 /* USER CODE BEGIN 1 */
 
