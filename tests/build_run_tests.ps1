6ebf7f4abc97d70d4c3f656ba447a97d6cf03b75
Marian Szabo <mszabo@coralwave.com>
2022-02-19 13:46:54 -0500

    updated cmake/test

--- tests/build_run_tests.ps1
+++ tests/build_run_tests.ps1
@@ -1,3 +1,4 @@
+Remove-Item -Recurse -Force build
 cmake -B build .
 cmake --build ./build  --target clean
 cmake --build ./build  --config Release
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,6 +1,7 @@
 //#include <catch2/reporters/catch_reporter_console.h>
 #include "catch_amalgamated.hpp"
 
+
 static int Factorial( int number ) {
    return number <= 1 ? number : Factorial( number - 1 ) * number;  // fail
 // return number <= 1 ? 1      : Factorial( number - 1 ) * number;  // pass
